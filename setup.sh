57d1dac245e3467f5a9179123493c0d25b1a695b
Suwings <suwings@outlook.com>
2021-12-31 21:26:04 +0800

    更新 脚本

---
+++ setup.sh
@@ -0,0 +1,198 @@
+#!/bin/bash
+printf "\033c"
+
+mcsmanager_install_path="/opt/mcsmanager"
+node_install_path="/opt/node-v14.17.6-linux-x64"
+
+Red_Error() {
+  echo '================================================='
+  printf '\033[1;31;40m%b\033[0m\n' "$@"
+  echo '================================================='
+  exit 1
+}
+ 
+Install_Node() {
+  echo "[x] rm -irf ${node_install_path}"
+  rm -irf ${node_install_path}
+
+  echo "[→] cd /opt || exit"
+  cd /opt || exit
+
+  echo "[↓] wget https://npm.taobao.org/mirrors/node/v14.17.6/node-v14.17.6-linux-x64.tar.gz"
+  wget https://npm.taobao.org/mirrors/node/v14.17.6/node-v14.17.6-linux-x64.tar.gz
+
+  echo "[↑] tar -zxf node-v14.17.6-linux-x64.tar.gz"
+  tar -zxf node-v14.17.6-linux-x64.tar.gz
+
+  echo "[x] rm -rf node-v14.17.6-linux-x64.tar.gz"
+  rm -rf node-v14.17.6-linux-x64.tar.gz
+
+  echo "[x] Delete the original Node link"
+  rm -f /usr/bin/npm
+  rm -f /usr/bin/node
+  rm -f /usr/local/bin/npm
+  rm -f /usr/local/bin/node
+
+  echo "[+] Creating a Node link"
+  ln -s ${node_install_path}/bin/npm /usr/bin/
+  ln -s ${node_install_path}/bin/node /usr/bin/
+  ln -s ${node_install_path}/bin/npm /usr/local/bin/
+  ln -s ${node_install_path}/bin/node /usr/local/bin/
+
+  echo "=============== Node Version ==============="
+  echo " node: $(node -v)"
+  echo " npm: $(npm -v)"
+  echo "=============== Node Version ==============="
+  echo
+  echo "[-] Node Installed Successfully!"
+  echo
+
+  sleep 3
+}
+
+Install_MCSManager() {
+
+  echo "[x] Delete the original MCSManager"
+  rm -irf ${mcsmanager_install_path}
+
+  echo "[+] mkdir -p ${mcsmanager_install_path}"
+  mkdir -p ${mcsmanager_install_path} || exit
+
+  echo "[→] cd ${mcsmanager_install_path}"
+  cd ${mcsmanager_install_path} || exit
+
+  echo "[↓] git clone https://gitee.com/mcsmanager/mcsmanager-daemon-production.git"
+  git clone https://gitee.com/mcsmanager/mcsmanager-daemon-production.git
+
+  echo "[-] mv mcsmanager-daemon-production daemon"
+  mv mcsmanager-daemon-production daemon
+
+  echo "[→] cd daemon"
+  cd daemon || exit
+
+  echo "[+] npm install --registry=https://registry.npm.taobao.org"
+  npm install --registry=https://registry.npm.taobao.org
+
+  echo "[←] cd .."
+  cd ..
+
+  echo "[↓] git clone https://gitee.com/mcsmanager/mcsmanager-web-production.git"
+  git clone https://gitee.com/mcsmanager/mcsmanager-web-production.git
+
+  echo "[-] mv mcsmanager-web-production web"
+  mv mcsmanager-web-production web
+
+  echo "[→] cd web"
+  cd web || exit
+
+  echo "[+] npm install --registry=https://registry.npm.taobao.org"
+  npm install --registry=https://registry.npm.taobao.org
+
+  echo "=============== MCSManager ==============="
+  echo " Daemon: ${mcsmanager_install_path}/daemon"
+  echo " Web: ${mcsmanager_install_path}/web"
+  echo "=============== MCSManager ==============="
+  echo
+  echo ""
+  echo -e "\033[1;32m[ok] MCSManager installed successfully!!!\033[0m"
+  echo "[ok] Location: ${mcsmanager_install_path}"
+  echo
+  sleep 3
+}
+
+Create_Service() {
+
+  echo "[x] Initialize the service file"
+  rm -f /etc/systemd/system/mcsm-daemon.service
+  rm -f /etc/systemd/system/mcsm-web.service
+
+  echo "[+] cat >>/etc/systemd/system/mcsm-daemon.service"
+  cat >>/etc/systemd/system/mcsm-daemon.service <<'EOF'
+[Unit]
+Description=MCSManager Daemon
+
+[Service]
+WorkingDirectory=/opt/mcsmanager/daemon
+ExecStart=/usr/bin/node app.js
+ExecReload=/bin/kill -s HUP $MAINPID
+ExecStop=/bin/kill -s QUIT $MAINPID
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  echo "[+] cat >>/etc/systemd/system/mcsm-web.service"
+  cat >>/etc/systemd/system/mcsm-web.service <<'EOF'
+[Unit]
+Description=MCSManager Web
+
+[Service]
+WorkingDirectory=/opt/mcsmanager/web
+ExecStart=/usr/bin/node app.js
+ExecReload=/bin/kill -s HUP $MAINPID
+ExecStop=/bin/kill -s QUIT $MAINPID
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  echo "[-] systemctl daemon-reload"
+  systemctl daemon-reload
+
+  echo "[+] systemctl enable mcsm-daemon.service --now"
+  systemctl enable mcsm-daemon.service --now
+
+  echo "[+] systemctl enable mcsm-web.service --now"
+  systemctl enable mcsm-web.service --now
+
+  sleep 4
+
+  echo "=================================================================="
+  echo -e "\033[1;32mWelcome to MCSManager\033[0m"
+  echo "=================================================================="
+  echo "Web Service Address:    http://localhost:23333"
+  echo "Daemon Service Address: http://localhost:24444"
+  echo "Username: root"
+  echo "Password: 123456"
+  echo -e "\033[33mYou must expose ports 23333 and 24444 to use the service properly on the Internet.\033[0m"
+  echo "=================================================================="
+  echo "systemctl restart mcsm-{daemon,web}.service"
+  echo "systemctl disable mcsm-{daemon,web}.service"
+  echo "systemctl enable mcsm-{daemon,web}.service"
+  echo "systemctl start mcsm-{daemon,web}.service"
+  echo "systemctl stop mcsm-{daemon,web}.service"
+  echo "=================================================================="
+
+}
+
+# ----------------- Program start ----------------- 
+
+# rm -f "$0"
+
+if [ $(whoami) != "root" ]; then
+  Red_Error "[x] Please use Root!"
+fi
+
+is64bit=$(getconf LONG_BIT)
+if [ "${is64bit}" != '64' ]; then
+  Red_Error "[x] Please use 64-bit system!"
+fi
+
+
+echo "+----------------------------------------------------------------------
+| MCSManager Installer
++----------------------------------------------------------------------
+| Copyright © 2021 Suwings All rights reserved.
++----------------------------------------------------------------------
+| Shell Install Script by Nuomiaa
++----------------------------------------------------------------------
+"
+
+echo "[+] Installing dependent software... (git,tar)"
+yum install -y git tar
+apt install -y git tar
+pacman -Syu --noconfirm git tar
+
+Install_Node
+Install_MCSManager
+Create_Service
---
+++ update_mcsm.bat
@@ -0,0 +1,47 @@
+@echo off
+
+Title MCSManager-Web 自动更新
+
+color 9
+echo ######################################################
+echo #                                                    #
+echo #               MCSManager-Web 自动更新              #
+echo #                  by 糯米(nuomiaa)                  #
+echo #                                                    #
+echo ######################################################
+echo.
+echo [-] 检查 Git 是否安装...
+
+for /F %%i in ('git --version') ^
+do (
+    set vars1=%%i
+)
+
+echo.
+
+if "%vars1%"=="git" (
+    echo [-] Git 已安装
+    echo.
+
+    echo [-] 初始化 Git
+    git init
+    git remote add origin https://gitee.com/mcsmanager/mcsmanager-web-production.git
+    git fetch --all
+    git reset --hard origin/master
+    echo.
+
+    echo [-] 正在拉取最新版本...
+    git pull origin master
+
+    echo [-] 更新完成！
+) else (
+    echo [x] Git 未安装，尝试使用 winget 安装...
+    winget install --id Git.Git -e --source winget
+
+    color 4
+    echo [-] 安装进程已结束，请重新运行脚本检查安装！
+    echo [-] 如 Git 安装失败，请手动安装 Git: https://git-scm.com/download/win
+)
+
+
+pause
