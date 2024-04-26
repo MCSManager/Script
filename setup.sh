fbd7d86d9cdf165e0895af83a5fd99e1b613f089
swwind <swwind233@gmail.com>
2024-04-26 14:39:46 +0800

    fix: change home directory to install path

--- setup.sh
+++ setup.sh
@@ -129,7 +129,7 @@ Create_Service() {
     userdel "$mcsmanager_user"
   fi
   
-  useradd -r -M -s "$(command -v nologin)" "$mcsmanager_user"
+  useradd -r -M -s "$(command -v nologin)" -d "$mcsmanager_install_path" "$mcsmanager_user"
   chown $mcsmanager_user:$mcsmanager_user -R "$mcsmanager_install_path"
 
   echo "[Unit]
