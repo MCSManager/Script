7ab22079afd4a291961cf38d49cbf043f9a9d511
swwind <swwind233@gmail.com>
2024-04-26 14:13:21 +0800

    add wget to deps list

--- setup.sh
+++ setup.sh
@@ -215,24 +215,24 @@ node_install_path="/opt/node-$node-linux-$arch"
 echo_cyan "[-] Architecture: $arch"
 
 # Install related software
-echo_cyan_n "[+] Installing dependent software (git, tar)... "
+echo_cyan_n "[+] Installing dependent software (git, tar, wget)... "
 if [[ -x "$(command -v yum)" ]]; then
-  yum install -y git tar
+  yum install -y git tar wget
 elif [[ -x "$(command -v apt-get)" ]]; then
-  apt-get install -y git tar
+  apt-get install -y git tar wget
 elif [[ -x "$(command -v pacman)" ]]; then
-  pacman -S --noconfirm git tar
+  pacman -S --noconfirm git tar wget
 elif [[ -x "$(command -v zypper)" ]]; then
-  zypper --non-interactive install git tar
+  zypper --non-interactive install git tar wget
 else
-  echo_red "[!] Cannot find your package manager! You may need to install git and tar manually!"
+  echo_red "[!] Cannot find your package manager! You may need to install git, tar and wget manually!"
 fi
 
 # Determine whether the relevant software is installed successfully
-if [[ -x "$(command -v git)" && -x "$(command -v tar)" ]]; then
+if [[ -x "$(command -v git)" && -x "$(command -v tar)" && -x "$(command -v wget)" ]]; then
   echo_green "Success"
 else
-  Red_Error "[x] Failed to find git and tar, please install them manually!"
+  Red_Error "[x] Failed to find git, tar and wget, please install them manually!"
 fi
 
 # Install the Node environment
