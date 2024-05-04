b8f03d759732e4552df697b3f208a39e4173d950
unknown <78304455+KevinLu2000@users.noreply.github.com>
2024-05-04 23:53:56 +0800

    add some helper functions

--- install_update.sh
+++ install_update.sh
@@ -11,3 +11,44 @@ arch=$(uname -m)
 USER="mcsm"
 COMMAND="all"
 
+# Helper Functions
+usage() {
+    echo "Usage: $0 [-u user] [-c command]"
+    echo "  -u  Specify the user (mcsm or root), default is 'mcsm'"
+    echo "  -c  Specify the command (web, daemon, or all), default is 'all'"
+    exit 1
+}
+Red_Error() {
+    echo '================================================='
+    printf '\033[1;31;40m%b\033[0m\n' "$@"
+    echo '================================================='
+    exit 1
+}
+echo_cyan() {
+    printf '\033[1;36m%b\033[0m\n' "$@"
+}
+echo_red() {
+    printf '\033[1;31m%b\033[0m\n' "$@"
+}
+
+echo_green() {
+    printf '\033[1;32m%b\033[0m\n' "$@"
+}
+
+echo_cyan_n() {
+    printf '\033[1;36m%b\033[0m' "$@"
+}
+
+echo_yellow() {
+    printf '\033[1;33m%b\033[0m\n' "$@"
+}
+
+# Check root permission
+check_sudo() {
+	if [ "$EUID" -ne 0 ]; then
+		echo "This script must be run as root. Please use \"sudo or root user\" instead."
+		exit 1
+	fi
+}
+check_sudo
+
