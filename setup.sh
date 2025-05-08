4805f359ce7a5808bf4e17d98959eda37577f091
KevinLu <kevin_git@asleague.org>
2025-05-08 00:50:54 -0400

    reformatted color style codes

--- setup.sh
+++ setup.sh
@@ -78,20 +78,29 @@ required_commands=(
 # Default to false, auto check later
 SUPPORTS_COLOR=false
 SUPPORTS_STYLE=false
-# ANSI escape codes for text formatting
+# Declare ANSI reset
 RESET="\033[0m"
-BOLD="\033[1m"
-UNDERLINE="\033[4m"
-ITALIC="\033[3m"  # Not supported on all terminals
 
 # Foreground colors
-FG_RED="\033[1;31m"
-FG_GREEN="\033[1;32m"
-FG_YELLOW="\033[1;33m"
-FG_BLUE="\033[1;34m"
-FG_MAGENTA="\033[1;35m"
-FG_CYAN="\033[1;36m"
-FG_WHITE="\033[1;37m"
+declare -A FG_COLORS=(
+  [black]="\033[0;30m"
+  [red]="\033[0;31m"
+  [green]="\033[0;32m"
+  [yellow]="\033[0;33m"
+  [blue]="\033[0;34m"
+  [magenta]="\033[0;35m"
+  [cyan]="\033[0;36m"
+  [white]="\033[0;37m"
+)
+
+# Font styles
+declare -A STYLES=(
+  [bold]="\033[1m"
+  [underline]="\033[4m"
+  [italic]="\033[3m"  # Often ignored on many terminals
+)
+
+
 ### Helper Functions
 
 ## Color Functions
